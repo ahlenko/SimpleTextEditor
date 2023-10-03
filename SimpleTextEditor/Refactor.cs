1e831340de275eacdf15f4d9158d28cd3504eae1
Талыбов Эльнур <elnyrtalybov@gmail.com>
2023-10-03 21:03:13 +0300

    Rename Method already done and ready to be used

--- SimpleTextEditor/Refactor.cs
+++ SimpleTextEditor/Refactor.cs
@@ -6,34 +6,86 @@ using System.Threading.Tasks;
 
 namespace SimpleTextEditor {
     public class Refactor {
-        public string RenameMethod(string text, string old_name, string new_name, bool comment) {
+
+        public string RenameMethod(string text, string old_name, string new_name, bool comment)
+        {
             // Розділимо текст на рядки для обробки кожного рядка окремо
             string[] lines = text.Split('\n');
             List<string> modifiedLines = new List<string>();
 
+            bool isMultiLineComment = false; // Прапор, що вказує на багаторядковий коментар
+
             foreach (string line in lines)
             {
-                // Перевіримо, чи містить рядок оголошення методу зі старою назвою
-                if (line.Contains(old_name + "("))
+                // Перевіримо, чи рядок починається з багаторядкового коментаря "/*"
+                if (line.Trim().StartsWith("/*"))
                 {
-                    // Замінимо стару назву на нову в рядку
-                    string modifiedLine = line.Replace(old_name + "(", new_name + "(");
-
-                    // Якщо було вказано, що слід змінювати коментарі, замінимо їх також
-                    if (comment)
-                    {
-                        modifiedLine = modifiedLine.Replace("// " + old_name, "// " + new_name);
-                    }
-
-                    modifiedLines.Add(modifiedLine);
+                    isMultiLineComment = true;
                 }
-                else
+
+                // Перевіримо, чи рядок закінчується багаторядковим коментарем "*/"
+                if (line.Trim().EndsWith("*/"))
                 {
-                    // Якщо рядок не містить оголошення методу зі старою назвою, залишимо його без змін
-                    modifiedLines.Add(line);
+                    isMultiLineComment = false;
+                }
+
+                // Якщо рядок не є багаторядковим коментарем, то ми можемо перевірити, чи містить він оголошення методу
+                if (!isMultiLineComment)
+                {
+                    // Перевіримо, чи містить рядок оголошення методу зі старою назвою
+                    if (line.Contains(old_name + "("))
+                    {
+                        if (line.Trim().StartsWith("//")) //Перевіримо, чи є рядок однорядковим коментарем
+                        {
+                            if (comment) //Якщо чекбокс відмічений
+                            {
+                                // Замінимо стару назву на нову в рядку
+                                string modifiedLine = line.Replace(old_name, new_name);
+                                modifiedLines.Add(modifiedLine);
+                            }
+                            else
+                            {
+                                // Якщо чекбокс не відімчений - залишаємо однорядковий коментар без змін
+                                modifiedLines.Add(line);
+                            }
+                        }
+                        else
+                        {
+                            // Замінимо стару назву на нову в рядку
+                            string modifiedLine = line.Replace(old_name, new_name);
+                            modifiedLines.Add(modifiedLine);
+                        }
+                    }
+                    else
+                    {
+                        // Якщо рядок не містить оголошення методу зі старою назвою, залишимо його без змін
+                        modifiedLines.Add(line);
+                    }
+                }
+
+
+                else
+                { // Якщо рядок є багаторядковим коментарем
+                    if (comment)
+                    { //відмічений чекбокс-замінюємо стару назву
+                        if (line.Contains(old_name + "("))
+                        {
+                            // Замінимо стару назву на нову в рядку
+                            string modifiedLine = line.Replace(old_name, new_name);
+                            modifiedLines.Add(modifiedLine);
+                        }
+                        else
+                        {
+                            // Якщо рядок не містить оголошення методу зі старою назвою, залишимо його без змін
+                            modifiedLines.Add(line);
+                        }
+                    }
+                    else
+                    { //то додаємо його без змін
+                        modifiedLines.Add(line);
+                    }
                 }
             }
-
             // Об'єднаємо модифіковані рядки назад у текст коду
             string modifiedText = string.Join("\n", modifiedLines);
             return modifiedText;
